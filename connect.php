b402cb86d79049152638641673e73d9cbf8b511f
Salman <59947811+elated-salman@users.noreply.github.com>
2023-06-08 19:51:55 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$hostname  = 'localhost';
-$username = 'root';
-$password='';
-$dbname = 'train';
+$hostname  = 'database-1.cssjsx9cgosf.us-east-1.rds.amazonaws.com';
+$username = 'admin';
+$password='admin123';
+$dbname = 'mydb';
 $connect =  mysqli_connect($hostname , $username , $password ,$dbname) or die("Error Connecting");
 ?>
